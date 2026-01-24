fe863ecaa78880963be75f7eecb3563a4e84a67f
jordi125229 <piotr.jedraszczyk12@gmail.com>
2026-01-24 13:38:37 +0100

    task 7

---
+++ src/methods/Task7.java
@@ -0,0 +1,14 @@
+package methods;
+
+class Task7 {
+    public static void main(String[] args) {
+        int x = 5;
+        zmienX(x);
+        System.out.println(x);
+    }
+
+    static void zmienX(int x){
+        x = 7;
+        System.out.println(x);
+    }
+}
