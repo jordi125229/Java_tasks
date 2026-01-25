185e90057c05774703dd6b7e4ebdfbb5848256de
jordi125229 <piotr.jedraszczyk12@gmail.com>
2026-01-25 11:33:56 +0100

    task 19

---
+++ src/methods/Task19.java
@@ -0,0 +1,68 @@
+package methods;
+
+import java.util.Scanner;
+
+class Task19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("Wybierz opcję:");
+            menu();
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    printNumbers();
+                    print(add(sc.nextInt(), sc.nextInt()));
+                    break;
+                case 2:
+                    printNumbers();
+                    print(reduct(sc.nextInt(), sc.nextInt()));
+                    break;
+                case 3:
+                    printNumbers();
+                    print(multiply(sc.nextInt(), sc.nextInt()));
+                    break;
+                case 0:
+                    System.out.println("Wyjście");
+                    break;
+                default:
+                    System.out.println("Błędny wydouble");
+            }
+        } while (option != 0);
+    }
+
+    static void menu() {
+        System.out.println("1 – Dodawanie");
+        System.out.println("2 – Odejmowanie");
+        System.out.println("3 – Mnożenie");
+        System.out.println("0 – Wyjście");
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static double reduct(double a, double b) {
+        return a - b;
+    }
+
+    static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    static void print(double i) {
+        System.out.println(i);
+    }
+    static void printNumbers(){
+        System.out.println("Podaj liczby do działania");
+    }
+}
+
+
+
+/*Menu kalkulatora**
+    - Napisz metodę `menu()` wyświetlającą opcje: `1 – Dodawanie`, `2 – Odejmowanie`, `3 – Mnożenie`, `0 – Wyjście`.
+    - Napisz osobne metody dla każdej operacji (`dodaj`, `odejmij`, `pomnoz`).
+    - W `main` obsłuż wybór użytkownika w pętli i wykonaj wybraną metodę.*/
